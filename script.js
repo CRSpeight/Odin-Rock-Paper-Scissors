0b96251e0103f0d55c13beda41a4ef25e2d78e74
Paradisio <paradisio15@gmail.com>
2022-11-06 22:53:03 -0600

    initial UI buildout

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const CHOICES = ['rock', 'paper', 'scissors'];
+const CHOICES = ["rock", "paper", "scissors"];
 
-game(5);
+// game(5);
 
-function game(numberOfRounds){
-    for (let i = 0; i < numberOfRounds; i++){
-        const playerSelection = prompt('Enter your selection');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+function game(numberOfRounds) {
+  for (let i = 0; i < numberOfRounds; i++) {
+    const playerSelection = prompt("Enter your selection");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
 }
 
-function playRound(playerSelection, computerSelection){
-    let playerChoice = playerSelection.toLowerCase();
-    while (!CHOICES.includes(playerChoice)){
-        playerSelection = prompt('Selection invalid, please try again');
-        playerChoice = playerSelection.toLowerCase();
-    }
+function playRound(playerSelection, computerSelection) {
+  let playerChoice = playerSelection.toLowerCase();
+  while (!CHOICES.includes(playerChoice)) {
+    playerSelection = prompt("Selection invalid, please try again");
+    playerChoice = playerSelection.toLowerCase();
+  }
 
-    if (playerChoice === computerSelection){
-        return `Tie! Both players picked ${playerChoice}`;
-    }
-    if (playerChoice === 'rock' && computerSelection == "scissors" ||
-    playerChoice === `paper` && computerSelection === "rock" ||
-    playerChoice === "scissors" && computerSelection === "paper"){
-        return `You win! ${playerChoice} beats ${computerSelection}`;
-    }
-    else{
-        return `You lose! ${computerSelection} beats ${playerChoice}`;
-    }
-    
-
-    
+  if (playerChoice === computerSelection) {
+    return `Tie! Both players picked ${playerChoice}`;
+  }
+  if (
+    (playerChoice === "rock" && computerSelection == "scissors") ||
+    (playerChoice === `paper` && computerSelection === "rock") ||
+    (playerChoice === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerChoice} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerChoice}`;
+  }
 }
-function getComputerChoice(){
-    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+function getComputerChoice() {
+  return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
